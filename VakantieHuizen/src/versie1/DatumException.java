387c46fd7c2b3da42ca5313a8c46ed66838773e9
kr15t <kr15t@DESKTOP-L25LR0S.dummy.porta.siemens.net>
2016-11-01 13:47:14 +0100

    back to basics

--- VakantieHuizen/src/versie1/DatumException.java
+++ VakantieHuizen/src/versie1/DatumException.java
@@ -1,40 +1,8 @@
 package versie1;
 
-public class DatumKonNietOpgehaaldWordenException extends Exception{
-	public DatumKonNietOpgehaaldWordenException(String errormessage){
+public class DatumException extends Exception{
+	public DatumException(String errormessage){
 		super(errormessage);
 	}
+
 }
-
-public class LeegDatumObjectException extends Exception{
-	public LeegDatumObjectException(String errormessage){
-		super(errormessage);
-	}
-}
-
-public class OngeldigJaarDatumObjectException extends Exception{
-	public OngeldigJaarDatumObjectException(String errormessage){
-		super(errormessage);
-	}
-}
-
-public class OngeldigeMaandDatumException extends Exception{
-	public OngeldigeMaandDatumException(String errormessage){
-		super(errormessage);
-	}
-}
-
-public class OngeldigeDagDatumException extends Exception{
-	public OngeldigeDagDatumException(String errormessage){
-		super(errormessage);
-	}
-}
-
-public class OngeldigeParameterDatumException extends Exception{
-	public OngeldigeParameterDatumException(String errormessage){
-		super(errormessage);
-	}
-}
-
-
-
